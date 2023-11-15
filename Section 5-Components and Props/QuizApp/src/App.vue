d8bbd167005f42c2a98e84663cc2eb0cae81530e
DUC Nguyen Viet (VTI.D10) <duc.nguyenviet@vti.vn>
2023-11-15 12:26:23 +0700

    add source section 5: Components and Props

---
+++ Section 5-Components and Props/QuizApp/src/App.vue
@@ -0,0 +1,72 @@
+<script setup>
+    import q from './data/quizes.json'
+    import {ref, watch} from 'vue'
+    import Card from './components/Card.vue'
+
+    const quizes = ref(q);
+    const search = ref("");
+
+    watch(search, () => {
+        quizes.value = q.filter(quizes => quizes.name.toLocaleUpperCase().includes(search.value.toLocaleUpperCase()))
+    })
+
+
+    // function filter() {
+    //     console.log("change state");
+    //     quizes.value = q.filter(quizes => quizes.name.toLocaleUpperCase().includes(search.value.toLocaleUpperCase()))
+    // }
+</script>
+
+<template>
+    <div class="container">
+        <header>
+            <h1>Quizes</h1>
+            <input v-model.trim="search" type="text">
+            <!-- <input v-model.trim="search" @input="filter" type="text"> -->
+        </header>
+        <div class="options-container">
+            <Card v-for="quiz in quizes" :key="quiz.id" :img="quiz.img" :name="quiz.name"/>
+            <!-- <div v-for="quiz in quizes" :key="quiz.id" class="card">
+                <img :src="quiz.img" alt="">
+                <div class="card-text">
+                    <h2>{{ quiz.name }}</h2>
+                    <p>{{ quiz.questions.length}} questions</p>
+                </div>
+            </div> -->
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .container {
+        max-width: 1000px;
+        margin: 0 auto
+    }
+
+    header {
+        margin-bottom: 10px;
+        margin-top: 30px;
+        display: flex;
+        align-items: center;
+    }
+
+    header h1 {
+        font-weight: bold;
+        margin-right: 30px;
+    }
+
+    header input {
+        border: none;
+        background-color: rgba(128, 128, 128, 0.1);
+        padding: 10px;
+        border-radius: 5px;
+    }
+
+    .options-container {
+        display:flex;
+        flex-wrap: wrap;
+        margin-top: 40px;
+    }
+
+    
+</style>
